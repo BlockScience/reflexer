9d48075d2bd9a2552b883afb0f32398bdf53ff47
Benjamin Scholtz <ben@bitsofether.com>
2020-10-13 11:38:34 +0200

    Misc. updates

--- models/system_model/model/parts/markets.py
+++ models/system_model/model/parts/markets.py
@@ -62,15 +62,16 @@ def resolve_debt_price(params, substep, state_history, state):
     if params[options.MarketPriceSource.__name__] == options.MarketPriceSource.EXTERNAL.value or params[options.DebtPriceSource.__name__] == options.DebtPriceSource.DISABLED.value:
         price_move = 0
 
-    return {'price_move':price_move}
+    return {'price_move': price_move}
 
 def update_debt_price(params, substep, state_history, state, policy_input):
 
     price_move = policy_input['price_move']
     value = state['debt_price'] + price_move
     key = 'debt_price'
-
-    return key,value
+    delta = params['delta_output'](key, state['timestep'])
+    
+    return key, value + delta
 
 def update_market_price(params, substep, state_history, state, policy_input):
     if params[options.MarketPriceSource.__name__] == options.MarketPriceSource.EXTERNAL.value:
@@ -79,23 +80,21 @@ def update_market_price(params, substep, state_history, state, policy_input):
         hat_error = state['error_hat']
         hat_integral = state['error_hat_integral']
         hat_derivative = state['error_hat_derivative']
-        hat_dp = params['kp-hat'] * hat_error + params['ki-hat'] * hat_integral + params['kd-hat'] * hat_derivative
+        hat_dp = params['kp-hat'] * hat_error + params['ki-hat'](state['timedelta']) * hat_integral + params['kd-hat'](state['timedelta']) * hat_derivative
 
         star_error = state['error_star']
         star_integral = state['error_star_integral']
         star_derivative = state['error_star_derivative']
-        star_dp = params['kp-star'] * star_error + params['ki-star'] * star_integral + params['kd-star'] * star_derivative
+        star_dp = params['kp-star'] * star_error + params['ki-star'](state['timedelta']) * star_integral + params['kd-star'](state['timedelta']) * star_derivative
 
         market_price = params['k0'] + params['k-autoreg-1']*state['market_price'] + star_dp + hat_dp
-        
-        if params[options.MarketPriceSource.__name__] == options.MarketPriceSource.HYBRID.value:
-            value = market_price + params['price_move'](state['timestep'])
-        else:
-            value = market_price
 
         if market_price < 0:
             value = 0
+        else:
+            value = market_price
 
     key = 'market_price'
-  
-    return key, value
+    delta = params['delta_output'](key, state['timestep'])
+    
+    return key, value + delta
