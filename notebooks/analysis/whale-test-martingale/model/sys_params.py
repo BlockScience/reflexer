6d63a6a9b38ebdacaac76a584a6b414fc25ba456
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2021-04-02 17:14:59 -0300

    add nbs for whale attack

---
+++ notebooks/analysis/whale-test-martingale/model/sys_params.py
@@ -0,0 +1,28 @@
+"""
+Model parameters
+"""
+
+import numpy as np
+
+initial_values = {
+    'WAD': 10**18,
+    'TOK':10**27,
+    'RAD': 10**45,
+    'SPY':31536000, # seconds per year
+    'MAX':2**255,
+    'SPH':60*60,
+}
+
+
+
+sys_params = {
+    'expected_lag': [10],
+    'minimum_period': [initial_values['SPH']],
+    'correction_wt': [0.5],
+    'noise_wt': [0.001],
+    'TOK':[initial_values['TOK']],
+    'fixed_price': [4],
+    'leakage': [.1/(60*60), .1/(60*60), 0, 0, 0],
+    'Kp':[10e-7],
+    'Ki':[-10e-9/(60*60), 10e-9/(60*60), -10e-9/(60*60), 10e-9/(60*60), 0]
+}
---
+++ notebooks/analysis/whale-test/model/partial_state_update_block.py
@@ -0,0 +1,40 @@
+from .parts.system import *
+
+partial_state_update_block = [
+    {
+        # system.py
+        'policies': 
+            {
+            'update_event': event
+            },
+        'variables': 
+            {
+            'timestamp':time_move,
+            'Dt': store_Dt
+            }
+    },
+    {
+        # system.py
+        'policies': 
+            {
+            'target_price': resolve_target_price,
+            'market_price': constant
+            },
+        'variables': 
+            {
+            'price':store_market_price,
+            'target': store_target_price,
+            'error': store_error,
+            'integral': update_leaky_integral,
+            }
+    },
+    {
+        'policies':
+            {
+            },
+        'variables':
+            {
+            'price_adjustment_rate': store_control_action
+            }   
+    }
+]
